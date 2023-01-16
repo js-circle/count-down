927f4c3223eb801a9ec83bd9688bca0fe0cac4b0
Basil Aslam <basilaslamnp@gmail.com>
2023-01-16 04:16:15 +0000

    fixed BUgs

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,26 @@
 // import './hooks/useCountDown' ;
 import ShowCounter from './countdowntimer'
 import React, { useState, useEffect } from 'react';
-import date from './date';
 import axios  from 'axios';
 
 import './App.css';
 
-// function App() {
-
-//   const NOW_IN_MS = new Date().getTime();
-//   console.log(LEFT_DAYS_IN_MS)
-//   return (
-//     <div className='underRoot'>
-//       <h1 className='glow'>Something amazing is on the way</h1>
-//       <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" alt="" />
-//       <CountdownTimer targetDate={LEFT_DAYS_IN_MS} />
-//     </div>
-//   );
-// }
+const END_DATE = new Date("Monday, 20 February 2023 12:00:00")
+const START_DATE = new Date()
+const DIFF_TIME =  Math.abs(START_DATE - END_DATE)
+const DAYS_LEFT = Math.ceil(DIFF_TIME / (1000 * 60 * 60 * 24)); 
+const LEFT_DAYS_IN_S = DAYS_LEFT * 24 * 60 * 60 ;
 
 function App() {
-  const [remainingTime, setRemainingTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(LEFT_DAYS_IN_S);
 
   useEffect(() => {
     // Get the remaining time from the server
-        // Get the remaining time from the server
+
 
   
       const intervalId = setInterval(() => {
         setRemainingTime(remainingTime - 1);
-        // Save the remaining time to the server
         axios
         .get('https://dateserver.onrender.com/api/remaining-time')
         .then(res => {
@@ -69,4 +60,3 @@ export default App;
 
 
 
-// export default App
